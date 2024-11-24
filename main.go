ef283407e90b4165228cded517d1979cd679e0d7
SwanHtetAungPhyo <swanhtetaungp@gmail.com>
2024-11-24 17:27:15 +0100

    folder struct changes

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
-
+	"time"
 	"github.com/golang-jwt/jwt/v4"
 
 )
@@ -19,6 +19,33 @@ func NewJWTMiddleware(secrectKey *string ) *GlobalJWTMiddleware{
 		Secret: *secrectKey,
 	}
 }
+
+type JWTManager struct {
+	SecretKey     string
+	TokenDuration time.Duration
+}
+
+
+func NewJWTManager(secretKey string, duration time.Duration) *JWTManager {
+	return &JWTManager{
+		SecretKey:     secretKey,
+		TokenDuration: duration,
+	}
+}
+
+func (j *JWTManager) GenerateToken(userID string, customClaims map[string]any) (string, error) {
+	claims := jwt.MapClaims{
+		"sub": userID,                     
+		"exp": time.Now().Add(j.TokenDuration).Unix(), 
+		"iat": time.Now().Unix(),    
+	}
+	for key, value := range customClaims {
+		claims[key] = value
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(j.SecretKey))
+}
+
 
 func (j * GlobalJWTMiddleware) Authorize(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
