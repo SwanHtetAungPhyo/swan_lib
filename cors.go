b99d231b3495e9e471220b76dc70e2823ae1503b
SwanHtetAungPhyo <swanhtetaungp@gmail.com>
2024-11-24 23:57:55 +0100

    folder change

--- cors.go
+++ cors.go
@@ -1,4 +1,4 @@
-package cors
+package swan_lib
 
 import (
 	"net/http"
@@ -6,9 +6,9 @@ import (
 )
 
 const (
-	ORIGIN_CONTROL = "Access-Control-Allow-Origin"
-	METHOD_CONTROL = "Access-Control-Allow-Methods"
-	HEADER_CONTROL = "Access-Control-Allow-Headers"
+	ORIGIN_CONTROL      = "Access-Control-Allow-Origin"
+	METHOD_CONTROL      = "Access-Control-Allow-Methods"
+	HEADER_CONTROL      = "Access-Control-Allow-Headers"
 	CREDENTIALS_CONTROL = "Access-Control-Allow-Credentials"
 )
 
@@ -27,35 +27,35 @@ func NewCORSMiddleware() *CORSMiddleware {
 	}
 }
 
-func (c *CORSMiddleware) AllowOrigins(origins []string) *CORSMiddleware{
+func (c *CORSMiddleware) AllowOrigins(origins []string) *CORSMiddleware {
 	c.AllowedOrigins = origins
-	return c 
+	return c
 }
 
-func (c *CORSMiddleware) AllowMethods(methods []string) *CORSMiddleware{
+func (c *CORSMiddleware) AllowMethods(methods []string) *CORSMiddleware {
 	c.AllowedMethods = methods
-	return c 
+	return c
 }
 
-func (c *CORSMiddleware) AllowHeaders(headers []string) *CORSMiddleware{
+func (c *CORSMiddleware) AllowHeaders(headers []string) *CORSMiddleware {
 	c.AllowedHeaders = headers
-	return c 
+	return c
 }
 
 func (c *CORSMiddleware) Handler(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request)  {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		origins := r.Header.Get("Origin")
 		if !c.isOriginAllowed(origins) {
 			w.WriteHeader(http.StatusForbidden)
 		}
 
-		w.Header().Set(ORIGIN_CONTROL,origins)
-		w.Header().Set(METHOD_CONTROL,strings.Join(c.AllowedMethods, ","))	
-		w.Header().Set(HEADER_CONTROL,strings.Join(c.AllowedHeaders, ","))
+		w.Header().Set(ORIGIN_CONTROL, origins)
+		w.Header().Set(METHOD_CONTROL, strings.Join(c.AllowedMethods, ","))
+		w.Header().Set(HEADER_CONTROL, strings.Join(c.AllowedHeaders, ","))
 		if c.AllowCredentials {
-			w.Header().Set(CREDENTIALS_CONTROL,"true")
+			w.Header().Set(CREDENTIALS_CONTROL, "true")
 		} else {
-			w.Header().Set(CREDENTIALS_CONTROL,"false")
+			w.Header().Set(CREDENTIALS_CONTROL, "false")
 		}
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusNoContent)
@@ -66,10 +66,9 @@ func (c *CORSMiddleware) Handler(next http.Handler) http.Handler {
 	})
 }
 
-
 func (c *CORSMiddleware) isOriginAllowed(origin string) bool {
-	for _, Allowedorigin :=range c.AllowedOrigins {
-		if Allowedorigin == "*" || Allowedorigin == origin{
+	for _, Allowedorigin := range c.AllowedOrigins {
+		if Allowedorigin == "*" || Allowedorigin == origin {
 			return true
 		}
 	}
